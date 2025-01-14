6047ef41d169c514c83e9514cb4fdcec1116cd0e
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2025-01-14 06:43:40 -0300

    add logout and create_user

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "default"
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
@@ -9,7 +9,6 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = "login"
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
@@ -25,5 +24,24 @@ def login():
             print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso."})
     return jsonify({"message": "Credenciais inválidas"}), 400
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso."})
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return jsonify({"message": "Usuário já cadastrado."}), 400
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso."})
+    return jsonify({"message": "Dados inválidos."}), 400
 if __name__ == "__main__":
     app.run(debug=True)
