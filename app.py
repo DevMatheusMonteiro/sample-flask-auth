076d97a553b2f00e89ddcc601aef13ef0d03dab2
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2025-01-14 21:23:12 -0300

    Using docker and mysql

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "default"
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud'
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
